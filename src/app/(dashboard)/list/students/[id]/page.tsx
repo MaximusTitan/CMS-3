9aa0b6bc845eddf03444cab128082f9a578aa7f7
yempativardhan <vardhan.y007@gmail.com>
2024-12-25 20:56:39 +0530

    Update page.tsx

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -137,7 +137,7 @@ const SingleStudentPage = async ({
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Student&apos;s Schedule</h1>
-          <BigCalendarContainer type="batchId" id={student.batch.id} />
+          <BigCalendarContainer type="batchId" id={student.batch.id.toFixed()} />
         </div>
       </div>
       {/* RIGHT */}
